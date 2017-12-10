d3a6b359bcbf5560701df482aaad5a285f69ad2a
phausamann <peter.hausamann@tum.de>
2017-12-10 20:32:56 +0100

    Add common.decorators

---
+++ sklearn_xarray/common/decorators.py
@@ -0,0 +1,48 @@
+""" ``sklearn_xarray.common.decorators`` """
+
+from sklearn_xarray import ClassifierWrapper as _ClassifierWrapper
+from sklearn_xarray import RegressorWrapper as _RegressorWrapper
+from sklearn_xarray import TransformerWrapper as _TransformerWrapper
+
+
+def classifier(cls):
+    """ Decorate an sklearn classifier with a wrapper. """
+
+    class ClassifierWrapper(_ClassifierWrapper):
+
+        def __init__(self, reshapes=None, sample_dim=None, compat=False,
+                     **fit_params):
+            super(ClassifierWrapper, self).__init__(
+                cls, reshapes=reshapes, sample_dim=sample_dim,
+                compat=compat, **fit_params)
+
+    return ClassifierWrapper
+
+
+def regressor(cls):
+    """ Decorate an sklearn regressor with a wrapper. """
+
+    class RegressorWrapper(_RegressorWrapper):
+
+        def __init__(self, reshapes=None, sample_dim=None, compat=False,
+                     **fit_params):
+            super(RegressorWrapper, self).__init__(
+                cls, reshapes=reshapes, sample_dim=sample_dim,
+                compat=compat, **fit_params)
+
+    return RegressorWrapper
+
+
+def transformer(cls):
+    """ Decorate an sklearn transformer with a wrapper. """
+
+    class TransformerWrapper(_TransformerWrapper):
+
+        def __init__(self, reshapes=None, sample_dim=None, compat=False,
+                     **fit_params):
+
+            super(TransformerWrapper, self).__init__(
+                cls, reshapes=reshapes, sample_dim=sample_dim,
+                compat=compat, **fit_params)
+
+    return TransformerWrapper
---
+++ sklearn_xarray/tests/test_common.py
@@ -0,0 +1,28 @@
+from sklearn.utils.estimator_checks import check_estimator
+from sklearn_xarray.common.wrappers import (
+    TransformerWrapper, ClassifierWrapper, RegressorWrapper)
+from sklearn_xarray.common.decorators import (
+    classifier, regressor, transformer)
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.svm import SVC
+
+
+def test_classifier():
+
+    check_estimator(ClassifierWrapper(SVC))
+    # TODO: check_estimator(classifier(SVC))
+
+
+def test_regressor():
+
+    check_estimator(RegressorWrapper(LinearRegression))
+    # TODO: check_estimator(regressor(LinearRegression))
+
+
+
+def test_transformer():
+
+    check_estimator(TransformerWrapper(StandardScaler))
+    # TODO: check_estimator(transformer(StandardScaler))
