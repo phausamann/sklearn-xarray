b69a278ea44c90e6b91116ad0782dddf517d0e59
phausamann <peter.hausamann@tum.de>
2018-08-06 17:18:29 +0200

    Update Travis config

--- build_tools/travis/make_docs.sh
+++ build_tools/travis/make_docs.sh
@@ -3,7 +3,7 @@ sudo apt-get -yq \
     --no-install-suggests --no-install-recommends --force-yes install \
     dvipng texlive-latex-base texlive-latex-extra
 
-conda install -y \
+conda install -y -c conda-forge \
     matplotlib sphinx pillow sphinx-gallery sphinx_rtd_theme numpydoc
 
 cd doc
