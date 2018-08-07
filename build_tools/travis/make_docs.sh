7f1978b74c4eefeaae4939f9d640f3c165384d2b
phausamann <peter.hausamann@tum.de>
2018-08-07 12:57:28 +0200

    Fix bug in Travis config

--- build_tools/travis/make_docs.sh
+++ build_tools/travis/make_docs.sh
@@ -4,6 +4,7 @@ cd doc
 set -o pipefail && make html 2>&1 | tee ~/log.txt
 cd ..
 
-cat log.txt && if grep -q "Traceback (most recent call last):" log.txt; then false; else true; fi
+cat ~/log.txt && if grep -q "Traceback (most recent call last):" ~/log.txt; \
+    then false; else true; fi
 
 cp .nojekyll doc/_build/html/.nojekyll
