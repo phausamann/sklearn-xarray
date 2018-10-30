e604ef8f82e80bb522a3fd072ffd258a0d35e4b9
phausamann <peter.hausamann@tum.de>
2018-10-30 12:01:34 +0100

    Fix failing tests with sklearn 0.20 Fixes #45

--- sklearn_xarray/tests/test_common.py
+++ sklearn_xarray/tests/test_common.py
@@ -8,8 +8,8 @@ import numpy.testing as npt
 from sklearn.utils.estimator_checks import check_estimator
 from sklearn_xarray import wrap
 
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler, KernelCenterer
+from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.svm import SVC
 
 from sklearn_xarray.tests.mocks import (
@@ -314,20 +314,19 @@ class PublicInterfaceTests(TestCase):
 
 def test_classifier():
 
-    svc = wrap(SVC)
-    check_estimator(svc)
-    assert hasattr(svc, 'predict')
-    assert hasattr(svc, 'decision_function')
+    lr = wrap(LogisticRegression, compat=True)
+    check_estimator(lr)
+    assert hasattr(lr, 'predict')
+    assert hasattr(lr, 'decision_function')
 
     svc_proba = wrap(SVC(probability=True))
-    check_estimator(svc_proba)
     assert hasattr(svc_proba, 'predict_proba')
     assert hasattr(svc_proba, 'predict_log_proba')
 
 
 def test_regressor():
 
-    lr = wrap(LinearRegression)
+    lr = wrap(LinearRegression, compat=True)
     check_estimator(lr)
     assert hasattr(lr, 'predict')
     assert hasattr(lr, 'score')
@@ -335,9 +334,11 @@ def test_regressor():
 
 def test_transformer():
 
+    tr = wrap(KernelCenterer, compat=True)
+    check_estimator(tr)
+    assert hasattr(tr, 'transform')
+
     ss = wrap(StandardScaler)
-    check_estimator(ss)
     assert hasattr(ss, 'partial_fit')
-    assert hasattr(ss, 'transform')
     assert hasattr(ss, 'inverse_transform')
     assert hasattr(ss, 'fit_transform')
